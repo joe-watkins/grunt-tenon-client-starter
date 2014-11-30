3a40de12af56ee10af571e6742afe4be48ef412b
Joe Watkins <joe@emergeinteractive.com>
2014-11-30 09:29:26 -0800

    1st commit

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+
+    watch: {
+      html : {
+        files : ["**/*.html"],
+        tasks : ["tenon"]
+      }
+  	}, // watch
+
+    tenon: {
+      options: {
+        key: 'you api key here',
+        filter: [31, 54],
+        level: 'AA'
+      },
+      all: {
+        options: {
+          saveOutputIn: 'allHtml.json',
+          snippet: true,
+          asyncLim: 2
+        },
+        src: ['index.html']
+      },
+      index: {
+        src: ['index.html']
+      }
+    }
+
+  }); // initConfig
+
+  grunt.registerTask('default', ["watch"]);
+
+};
---
+++ README.md
@@ -0,0 +1,2 @@
+#Grunt Tenon Starter
+This is a simple starter package to get up and running with Grunt Tenon. Be sure to signup for a free account at [Tenon.io](http://www.tenon.io/register.php) to obtain the API key needed for using this Grunt plugin.
